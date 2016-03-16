99f6cdd5f9c6a30444108bd32c49f660b10a0660
Bernard Anderson <bernard.anderson@gmail.com>
2016-03-16 15:48:27 -0500

    added bootstrap columns/cards and populating the cards with car data and colors

--- js/EventHandlers.js
+++ js/EventHandlers.js
@@ -1,6 +1,11 @@
 var CarLot = (function(eventHandlers) {
 
   eventHandlers.activateEvents = function() {
+    var carCardHolder = document.querySelector("#car-card-holder");
+
+    carCardHolder.addEventListener("click", function(event) {
+    })
+
   }
 
   return eventHandlers;
--- js/quiz.js
+++ js/quiz.js
@@ -1,11 +1,37 @@
 function populatePage(sentInventory) {
 
-  console.log(sentInventory);
+  var carCardString = "";
 
   // Loop over the inventory and populate the page
+  for (var i = 0; i < sentInventory.length; i++) {
+    var carCardHolder = document.querySelector("#car-card-holder");
+    var carCard = document.createElement("div");
+    carCard.id = `car-card-${i}`;
+    carCard.classList.add("col-md-4");
+    carCard.classList.add("bg-warning");
+    carCardHolder.appendChild(carCard);
+
+    curCar = sentInventory[i];
+
+    carCardString = `<h3>${curCar["year"]} ${curCar["make"]} ${curCar["model"]}</h3>`;
+    carCardString += `<p>Yours for only: $${curCar["price"]}</p>`;
+
+    carCard.setAttribute("style", `border: 3px solid ${curCar['color']}`);
+    carCardString += `<p>This car comes in beautiful <span style="background:${curCar['color']}">${curCar['color']}</p>`;
+
+    if (curCar["purchased"] === true) {
+      carCardString += `<p>We're sorry but this car is NOT available</p>`;
+    } else {
+      carCardString += `<p>BUY NOW! Supplies are limited!</p>`;
+    };
+
+    carCardString += `Description: ${curCar['description']}`;
+    carCard.innerHTML += carCardString;
+  };
+
 
   // Now that the DOM is loaded, establish all the event listeners needed
-  // CarLot.activateEvents();
+  CarLot.activateEvents();
 }
 
 // Load the inventory and send a callback function to be
