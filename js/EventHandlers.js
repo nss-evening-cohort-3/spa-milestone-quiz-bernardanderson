2aa15b3db9dc355c20b19e47f78bd48c1c191ee1
Bernard Anderson <bernard.anderson@gmail.com>
2016-03-16 23:48:40 -0500

    added one event listener to change the car card border

--- js/EventHandlers.js
+++ js/EventHandlers.js
@@ -1,11 +1,29 @@
 var CarLot = (function(eventHandlers) {
 
   eventHandlers.activateEvents = function() {
-    var carCardHolder = document.querySelector("#car-card-holder");
+
+    let carCardHolder = document.querySelector("#car-card-container");
+
+    let listOfCarCards = document.getElementsByClassName("round-card");
 
     carCardHolder.addEventListener("click", function(event) {
-    })
+      console.log (event.target, event.currentTarget);
 
+      event.stopPropagation();
+      if (event.target !== event.currentTarget) {
+        for (let i = 0; i < listOfCarCards.length; i++) {
+          if (listOfCarCards[i].style.borderWidth === "6px") {
+            listOfCarCards[i].style.borderWidth = "3px";
+          };
+        };
+
+        for (let i = 0; i < event.path.length; i++) {
+          if (event.path[i].id === "car-card-container") {
+            event.path[i-2].style.borderWidth = "6px";
+          } 
+        };
+      }
+    });
   }
 
   return eventHandlers;
--- js/quiz.js
+++ js/quiz.js
@@ -1,39 +1,65 @@
 function populatePage(sentInventory) {
 
+  // Gets the main container div for where the cards will be stored
+  var carCardContainer = document.getElementById("car-card-container");
+
+  // Initializes the string where each cars' information will be stored
   var carCardString = "";
 
-  // Loop over the inventory and populate the page
+  // Loop over each car in the inventory and populate the page
   for (var i = 0; i < sentInventory.length; i++) {
-    var carCardHolder = document.querySelector("#car-card-holder");
+
+    // Starting at 0, for every three cars cards in the inventory
+    //  a new div with the class of "row" will be add to the DOM.
+    //  This keeps columns working on my tiny laptop screen 
+    if (i % 3 === 0) {
+      var carCardRow = document.createElement("div");
+      carCardRow.classList.add("row");
+      carCardContainer.appendChild(carCardRow);
+    }
+
+    // Builds the card for each car
     var carCard = document.createElement("div");
     carCard.id = `car-card-${i}`;
     carCard.classList.add("col-md-4");
+    carCard.classList.add("round-card");
     carCard.classList.add("bg-warning");
-    carCardHolder.appendChild(carCard);
+    carCardRow.appendChild(carCard);
 
+    // This just sets the "current car" element in the array to a shorter 
+    //  easier to access variable.
     curCar = sentInventory[i];
 
+    // Makes the Car Title string
     carCardString = `<h3>${curCar["year"]} ${curCar["make"]} ${curCar["model"]}</h3>`;
+
+    // Adds the price info to the string
     carCardString += `<p>Yours for only: $${curCar["price"]}</p>`;
 
+    // Sets the car card border color to the color of the car and also
+    //  adds the color info to the car string
     carCard.setAttribute("style", `border: 3px solid ${curCar['color']}`);
     carCardString += `<p>This car comes in beautiful <span style="background:${curCar['color']}">${curCar['color']}</p>`;
 
+    // This checks to see if the car is purchased and sets the 
+    //  string added to the car string appropriately
     if (curCar["purchased"] === true) {
       carCardString += `<p>We're sorry but this car is NOT available</p>`;
     } else {
       carCardString += `<p>BUY NOW! Supplies are limited!</p>`;
     };
 
-    carCardString += `Description: ${curCar['description']}`;
+    // Adds the car description to the car string.
+    carCardString += `<p>Description: ${curCar['description']}</p>`;
+
+    // Inserts the car string into the current car card.
     carCard.innerHTML += carCardString;
   };
 
-
-  // Now that the DOM is loaded, establish all the event listeners needed
+  // The event listeners once the DOM is loaded and populated
   CarLot.activateEvents();
 }
 
-// Load the inventory and send a callback function to be
-// invoked after the process is complete
+// Loads the inventory, and sends the callback function 
+//  to populatePage
 CarLot.loadInventory(populatePage);
